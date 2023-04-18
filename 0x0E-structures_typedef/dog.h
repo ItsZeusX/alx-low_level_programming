c433a3413800a4008f34e526026ab1aba3102b7e
ItsZeusX <youssefelkhattabe1@gmail.com>
2023-04-17 18:21:33 -0700

    Dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,12 @@
 
 #include <stdio.h>
 
-
+/**
+ * struct dog - a new type with 3 elements
+ * @name: name of the dog
+ * @owner: the owner of the dog
+ * @age: the age of the dog
+ */
 
 typedef struct dog
 {
@@ -19,15 +24,24 @@ void print_dog(struct dog *d);
 
 dog_t *new_dog(char *name, float age, char *owner);
 
-
+/**
+ * _strlen - returns  the length of a string
+ * @s: is the string to check the length
+ * Return: returns the length of a string
+ */
 
 int _strlen(char *s);
 
-
+/**
+ * _strcpy - copies a string
+ * @dest: the destination to be copied to
+ * @src: the source to copy from
+ * Return: the pointer to dest
+ */
 
 char *_strcpy(char *dest, char *src);
 
 
 void free_dog(dog_t *d);
 
-#endif 
+#endif /* dog.h */
