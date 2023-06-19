1d8363950f1757081b0dbd78bdcac260887dc9d4
ItsZeusX <youssefelkhattabe1@gmail.com>
2023-06-19 08:39:28 -0700

    2 Strchr.c

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character string
+ *@s: points to a character
+ *@c: character
+ * Return: nulil
+ */
+char *_strchr(char *s, char c)
+{
+	int i, j = 0;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i <= j; i++)
+	{
+		if (s[i] == c)
+		{
+			s = s + i;
+			return (s);
+		}
+	}
+	return ('\0');
+}
